603be29e6f6622080297023acedc2f78ba9c090f
Anton Stroka <deestroker@MacBook-Pro-DeeStroKeR.local>
2020-04-13 13:25:05 +0200

    Updates

---
+++ precache-manifest.46431b3fcee7e4d31821fbaae640bb2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058f773ffc36c14ce343086a86ccd04f",
+    "url": "/header/index.html"
+  },
+  {
+    "revision": "b51a3b7bc4f805dc8d26",
+    "url": "/header/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "aed962c117cc81cc1e6f",
+    "url": "/header/static/css/main.e970b019.chunk.css"
+  },
+  {
+    "revision": "b51a3b7bc4f805dc8d26",
+    "url": "/header/static/js/2.63e8017d.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/header/static/js/2.63e8017d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed962c117cc81cc1e6f",
+    "url": "/header/static/js/main.f7d289aa.chunk.js"
+  },
+  {
+    "revision": "e2879cd9f9fafe040126",
+    "url": "/header/static/js/runtime-main.c6c46790.js"
+  }
+]);
